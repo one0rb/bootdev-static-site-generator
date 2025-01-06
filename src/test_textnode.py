b4f8b8c5914abc43efbadb510e702dea137142ca
Andrew Ball <one0rb.git@gmail.com>
2025-01-07 07:52:31 +0800

    Add tests for TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq_no_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_eq_false_diff_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false_diff_texttype(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false_diff_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://one0rb.github.io")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, bold, https://boot.dev)", repr(node)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
