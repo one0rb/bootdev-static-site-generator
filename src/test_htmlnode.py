31f9d05467f0185721d36167efe8c9af2cd439f4
Andrew Ball <one0rb.git@gmail.com>
2025-01-10 23:57:56 +0800

    Add LeafNode subclass & tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_values(self):
@@ -25,6 +25,30 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             "HTMLNode(h3, This is a header, children: None, {'color': 'red'})",
             repr(node)
+        )
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "Hello world!", {"class": "greeting"})
+        self.assertEqual(
+            '<p class="greeting">Hello world!</p>', node.to_html()
+        )
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello world!")
+        self.assertEqual(
+            'Hello world!', node.to_html()
+        )
+
+    def test_to_html_error(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_repr(self):
+        node = LeafNode("p", "Hello world!", {"class": "greeting"})
+        self.assertEqual(
+            "LeafNode(p, Hello world!, {'class': 'greeting'})", repr(node)
         )
 
 if __name__ == "__main__":
